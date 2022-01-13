d4e7944e725b1e291ba170ee24092e544d96c95c
amolnagose12 <amolnagose12@gmail.com>
2022-01-13 12:53:11 +0530

    second commit

--- src/com/demo/HelloWorld.java
+++ src/com/demo/HelloWorld.java
@@ -3,6 +3,7 @@ package com.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hi Amol");
     }
 
 }
